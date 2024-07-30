a4e6ffa51d58e7e0f1313cc998cbc7cb28e58288
panicbit <panicbit.dev@gmail.com>
2024-07-30 22:07:18 +0200

    migrate to latest nushell plugin version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 
-use nu_plugin::{serve_plugin, EvaluatedCall, LabeledError, MsgPackSerializer};
-use nu_protocol::{PluginSignature, ShellError, Span, SyntaxShape, Value};
+use nu_plugin::{
+    serve_plugin, EvaluatedCall, MsgPackSerializer, PluginCommand, SimplePluginCommand,
+};
+use nu_protocol::{LabeledError, ShellError, Signature, Span, SyntaxShape, Value};
 
 fn main() {
     serve_plugin(&mut PgPlugin::new(), MsgPackSerializer);
@@ -16,16 +18,38 @@ impl PgPlugin {
 }
 
 impl nu_plugin::Plugin for PgPlugin {
-    fn signature(&self) -> Vec<PluginSignature> {
-        let signature =
-            PluginSignature::build("pg").required("query", SyntaxShape::String, "query to execute");
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
 
-        vec![signature]
+    fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
+        vec![Box::new(PgCommand)]
+    }
+}
+
+struct PgCommand;
+
+impl SimplePluginCommand for PgCommand {
+    type Plugin = PgPlugin;
+
+    fn name(&self) -> &str {
+        "pg"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::new("pg")
+            .add_help()
+            .required("query", SyntaxShape::String, "query to execute")
+    }
+
+    fn usage(&self) -> &str {
+        ""
     }
 
     fn run(
-        &mut self,
-        name: &str,
+        &self,
+        plugin: &PgPlugin,
+        engine: &nu_plugin::EngineInterface,
         call: &EvaluatedCall,
         input: &Value,
     ) -> Result<Value, LabeledError> {
@@ -51,14 +75,16 @@ impl Args {
 fn load_config() -> Result<postgres::Config, ShellError> {
     let pg_url = env_var("PG_URL")?;
     let config = pg_url.parse::<postgres::Config>().map_err(|err| {
-        ShellError::GenericError(
-            err.to_string(),
-            "The allowed syntax for `PG_URL` can be found at\nhttps://docs.rs/postgres/0.19.7/postgres/config/struct.Config.html#url".into(),
-            None,
-            None,
-            vec![],
-        )
+        ShellError::GenericError {
+            error: err.to_string(),
+            msg: "The allowed syntax for `PG_URL` can be found at\nhttps://docs.rs/postgres/0.19.7/postgres/config/struct.Config.html#url".into(),
+            span: None,
+            help: None,
+            inner: vec![],
+         }
     })?;
+
+    Ok(config)
 }
 
 fn env_var(name: &str) -> Result<String, ShellError> {
